b232c457f3f1b0a67a23eaafd5eef37e735be695
jricica <janricica@ufm.edu>
2025-07-12 18:11:00 -0600

    package.json

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "/Bittrip/", // 👈 usa el nombre EXACTO del repo
 });
